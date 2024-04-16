8c6430ec60d9da6c10643746f47fff77dc5d862f
Konstantin Zhukov <kvzhkv@gmail.com>
2024-04-16 11:05:03 +0200

    add shared styled components

---
+++ src/components/styled.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+    background: transparent;
+    color: inherit;
+    width: 3rem;
+    height: 3rem;
+    font-size: 2rem;
+    border: none;
+    margin: 0;
+    padding: 0;
+    cursor: pointer;
+
+    &:hover {
+        color: #646cff;
+    }
+
+    &:disabled {
+        color: #333333;
+        cursor: default;
+    }
+`;
+
+export const ButtonsBlock = styled.div`
+    width: 100%;
+    margin-top: 1rem;
+    display: flex;
+    justify-content: space-between;
+`;
